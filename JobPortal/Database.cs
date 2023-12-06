abb9bc93e814dd07c76cbb8e4d180c3e023aeba0
pawel <pawel.marcisz1@gmail.com>
2023-12-06 19:43:13 +0100

    Added Database

---
+++ JobPortal/Database.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal
+{
+    public class Database
+    {
+        public static void CreateDb()
+        {
+            string dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "contacts.db");
+
+            using (var db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                string tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS contacts (contact_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, phone TEXT, email TEXT)";
+
+                var createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
